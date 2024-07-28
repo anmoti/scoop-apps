2f2169d3b700b20bcfcb0d734ca09ecde0bd32cf
Anmoti <anmoti.my@gmail.com>
2024-07-28 10:56:40 +0900

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "anmoti/scoop-apps:main"
+    [String]$upstream = "anmoti/scoop-apps:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
